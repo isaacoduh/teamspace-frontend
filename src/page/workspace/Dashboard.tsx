c679d99eb2bccbdb9d043bc070778085b5ea35df
Isaac Oduh <ufuomaoduh@gmail.com>
2025-03-12 10:02:03 +0000

    feat: implement dashboard placeholders

--- src/page/workspace/Dashboard.tsx
+++ src/page/workspace/Dashboard.tsx
@@ -1,8 +1,56 @@
+import { Plus } from "lucide-react";
+import { Button } from "../../components/ui/button";
+import useCreateProjectDialog from "../../hooks/use-create-project-dialog";
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "../../components/ui/tabs";
+
 const WorkspaceDashboard = () => {
+  const { onOpen } = useCreateProjectDialog();
   return (
-    <main>
-      <div>
-        <div>Workspace Overview</div>
+    <main className="flex flex-1 flex-col py-4 md:pt-3">
+      <div className="flex items-center justify-between space-y-2 mb-6">
+        <div>
+          {" "}
+          <h2 className="text-2xl font-bold tracking-tight">
+            Workspace Overview
+          </h2>
+          <p className="text-muted-foreground">
+            Here&apos;s an overview for this workspace!
+          </p>
+        </div>
+        <Button onClick={onOpen}>
+          <Plus />
+          New Project
+        </Button>
+      </div>
+      <div className="">Workspace Analytics</div>
+      <div className="mt-4">
+        <Tabs defaultValue="projects" className="w-full border rounded-lg p-2">
+          <TabsList className="w-full justify-start border-0 bg-gray-50 px-1 h-12">
+            <TabsTrigger className="py-2" value="projects">
+              Recent Projects
+            </TabsTrigger>
+            <TabsTrigger className="py-2" value="tasks">
+              Recent Tasks
+            </TabsTrigger>
+            <TabsTrigger className="py-2" value="members">
+              Recent Members
+            </TabsTrigger>
+          </TabsList>
+          <TabsContent value="projects">
+            <p>Recent Projects</p>
+          </TabsContent>
+          <TabsContent value="tasks">
+            <p>Recent Tasks</p>
+          </TabsContent>
+          <TabsContent value="members">
+            <p>Recent Members</p>
+          </TabsContent>
+        </Tabs>
       </div>
     </main>
   );
