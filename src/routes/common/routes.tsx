2dc30f163b2ebbe715fd512a470618f2bc14c01d
Isaac Oduh <ufuomaoduh@gmail.com>
2025-04-07 06:16:10 +0100

    feat: implement tasks page

--- src/routes/common/routes.tsx
+++ src/routes/common/routes.tsx
@@ -2,6 +2,7 @@ import SignIn from "../../page/auth/SignIn";
 import SignUp from "../../page/auth/SignUp";
 import WorkspaceDashboard from "../../page/workspace/Dashboard";
 import ProjectDetails from "../../page/workspace/ProjectDetails";
+import Tasks from "../../page/workspace/Tasks";
 import { AUTH_ROUTES, PROTECTED_ROUTES } from "./routePaths";
 
 export const authenticationRoutes = [
@@ -12,6 +13,7 @@ export const authenticationRoutes = [
 export const protectedRoutePaths = [
   { path: PROTECTED_ROUTES.WORKSPACE, element: <WorkspaceDashboard /> },
   { path: PROTECTED_ROUTES.PROJECT_DETAILS, element: <ProjectDetails /> },
+  { path: PROTECTED_ROUTES.TASKS, element: <Tasks /> },
 ];
 
 export const baseRoutePaths = [
